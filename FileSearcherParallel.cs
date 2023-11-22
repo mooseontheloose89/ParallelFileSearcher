0e0786b6ea8d9c889a5b72ed9574d6d2292a6ffc
Harry M <mooseontheloose89@gmail.com>
2023-11-22 01:40:56 +0000

    Add project files.

---
+++ FileSearcherParallel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelFileSearcher
+{
+    public class FileSearcherParallel
+    {
+        public ConcurrentBag<string> FindFilesWithExtension(string rootPath, string extension)
+        {
+            var files = new ConcurrentBag<string>();
+            
+            try
+            {
+                var directFiles = Directory.GetFiles(rootPath, $"*{extension}");
+                foreach (var file in directFiles)
+                {
+                    files.Add(file);
+                }
+                Console.WriteLine($"Direct files found in root: {directFiles.Length}");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Error accessing root path {rootPath}: {ex.Message}");
+            }
+
+
+            foreach (var dir in Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories))
+            {
+                try
+                {
+
+                    foreach (var file in Directory.GetFiles(dir, $"*{extension}"))
+                    {
+                        files.Add(file);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex}: Access denied to {dir}. Skipping...");
+                }
+            }           
+
+            return files;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using ParallelFileSearcher; 
+
+class Program
+{
+    static IConfiguration Configuration { get; set; }
+
+    static void Main(string[] args)
+    {
+        
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        Configuration = builder.Build();
+
+        
+        var directoryPath = Configuration["FileSearchConfig:DirectoryPath"];
+        var fileExtension = Configuration["FileSearchConfig:FileExtension"];
+
+        
+        var searcher = new FileSearcherParallel();
+        var foundFiles = searcher.FindFilesWithExtension(directoryPath, fileExtension);
+
+        
+        foreach (var file in foundFiles)
+        {
+            Console.WriteLine(file);
+        }
+
+        Console.WriteLine($"{foundFiles.Count} files found with extension {fileExtension}.");
+    }
+}
